e155bd9545bf8df0e7761d89b041a39d881f450a
khalid alharbi <85522648+alrieq@users.noreply.github.com>
2022-11-08 15:45:29 +0300

    Add files via upload

---
+++ src/main/java/edu/kau/fcit/cpit252/receiptStrategy/EmailReceipt.java
@@ -0,0 +1,37 @@
+package edu.kau.fcit.cpit252.receiptStrategy;
+
+import edu.kau.fcit.cpit252.paymentsStrategy.Payment;
+import edu.kau.fcit.cpit252.shopping.Product;
+import edu.kau.fcit.cpit252.utils.EmailTemplate;
+import edu.kau.fcit.cpit252.utils.SendEmail;
+
+import java.util.List;
+
+public class EmailReceipt extends Receipt{
+    public EmailReceipt(String subject, String recipient , List<Product> products) {
+        super(products);
+        //TODO Auto-generated constructor stub
+        this.subject = subject;
+        this.recipient = recipient;
+    }
+
+
+    private String subject;
+    private String recipient;
+
+
+    @Override
+    public void generate() {
+        EmailTemplate template = new EmailTemplate();
+        String message =  template.getEmailTemplate("Our Neighborhood Bakery", this.getId(), this.getIssueDate(),
+                this.getProducts());
+        try {
+            SendEmail.send(this.subject, message, this.recipient);
+        }
+        catch(Exception e){
+            System.err.println("Failed to email the receipt: Reason: " + e.getMessage());
+            return;
+        }
+
+    }
+}
